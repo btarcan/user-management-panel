8f3b24511aa71b84409b4ff322e8de163442b754
Bora Tarcan <bora.tarcan@outlook.com>
2026-04-27 08:28:46 +0300

    fix: add barrel export for components and pages

---
+++ src/components/users/index.ts
@@ -0,0 +1,6 @@
+export { UserFilters } from './UserFilters';
+export { UserTable } from './UserTable';
+export { UserForm } from './UserForm';
+export { DeleteConfirmDialog } from './DeleteConfirmDialog';
+export { Pagination } from './Pagination';
+export { StatsBar } from './StatsBar';
